0c0427b996a3d8078d4aa6dac4be8f1fbe7a4e70
Espinozaje <jeisson12aaron@gmail.com>
2026-01-14 14:56:14 -0500

    fix rutas

--- src/routes/ventaRoutes.js
+++ src/routes/ventaRoutes.js
@@ -1,10 +1,11 @@
+// src/routes/ventaRoutes.js
 const express = require('express');
 const router = express.Router();
 const ventaController = require('../controllers/VentaController');
 
-router.get('/', ventaController.getVentas);
-router.get('/ventas/stats', ventaController.getStats);
-router.get('/config/productos', ventaController.getConfig);
-router.post('/', ventaController.guardarVenta);
+router.get('/', ventaController.getVentas);          
+router.get('/stats', ventaController.getStats);      
+router.get('/config/productos', ventaController.getConfig); 
+router.post('/', ventaController.guardarVenta);   
 
 module.exports = router;
